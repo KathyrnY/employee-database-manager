4c22e24bda3572f9a0a907c9d3abfb4830e28e76
unknown <kathyrnyoung57@gmail.com>
2023-06-10 22:19:55 -0400

    ensured db connection and added start function

--- index.js
+++ index.js
@@ -7,3 +7,60 @@ const db = mysql.createConnection({
     database: 'employees_db'
 });
 
+db.connect(err => {
+    if (err) throw err;
+    console.log('Connected');
+    start();
+});
+
+async function start() {
+    const inquirer = await import('inquirer');
+    inquirer
+        .default.prompt([
+            {
+                type: "list",
+                name: "databaseTask",
+                message: "What would you like to do?",
+                choices: [
+                    "View All Employees",
+                    "Add Employee",
+                    "Update Employee Role",
+                    "View All Roles",
+                    "Add Role",
+                    "View All Departments",
+                    "Add Department",
+                    "Quit",
+                ]
+            }
+        ])
+        .then((answers) => {
+            const { databaseTask } = answers;
+            switch (databaseTask) {
+                case "View All Employees":
+                    // viewAllEmployees();
+                    break;
+                case "Add Employee":
+                    // addEmployee();
+                    break;
+                case "Update Employee Role":
+                    // updateEmployeeRole();
+                    break;
+                case "View All Roles":
+                    // viewAllRoles();
+                    break;
+                case "Add Role":
+                    // addRole();
+                    break;
+                case "View All Departments":
+                    // viewAllDepartments();
+                    break;
+                case "Add Department":
+                    // addDepartment();
+                    break;
+                case "Quit":
+                    db.end();
+                    console.log('Exited program');
+                    break;
+            }
+        });
+}
