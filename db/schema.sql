a82cab0c95da82fd2f76008f2c2eebeb9a2f4281
unknown <kathyrnyoung57@gmail.com>
2023-06-10 22:06:48 -0400

    added seed data and dependencies

--- db/schema.sql
+++ db/schema.sql
@@ -13,9 +13,7 @@ CREATE TABLE role (
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT,
-    FOREIGN KEY (department_id)
-    REFERENCES department(id)
-    ON DELETE NOT NULL
+    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
 
 CREATE TABLE employee (
@@ -24,6 +22,6 @@ CREATE TABLE employee (
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
     manager_id INT NULL,
-    FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL
+    FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
     FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const inquirer = require('inquirer');
 const mysql = require('mysql2');
+
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '',
-    database: ''
-})
+    password: '12345678@',
+    database: 'employees_db'
+});
 
